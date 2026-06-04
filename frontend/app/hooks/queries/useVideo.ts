d1df3ff4c38f22ddb0ab9a033ffff510575f2e1b
Jur Beijaard <jurspammer@gmail.com>
2026-06-04 21:29:25 +0200

    Refetch every 10 seconds

--- frontend/app/hooks/queries/useVideo.ts
+++ frontend/app/hooks/queries/useVideo.ts
@@ -9,7 +9,7 @@ export const videoQueryOptions = (id: string) =>
     refetchInterval: (query) => {
       const status = query.state.data?.status;
       if (status === VIDEO_STATUS.FINISHED || status === VIDEO_STATUS.FAILED) return false;
-      return 2000;
+      return 10000; // 10 Seconds
     },
   });
 
